7e00c1ff1eef96c9f632841843d0ca79506bf2ce
xyh <2928494045@qq.com>
2023-05-04 13:34:07 +0800

    demo0.8.1

--- src/main/java/com/cy/store/config/WebMvcConfig.java
+++ src/main/java/com/cy/store/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/file/**").addResourceLocations("file:/Users/xyh/Downloads/毕设/开发/pic/furniture/");
+        registry.addResourceHandler("/file/**").addResourceLocations("file:/Users/xyh/Downloads/毕设/开发/furniture/src/main/resources/static/pic/");
     }
 
 }
